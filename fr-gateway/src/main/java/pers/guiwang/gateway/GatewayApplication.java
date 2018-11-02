d490d851027fdda8c7fc2d71e826c6fa1284ad30
Guiwang <guixiong97@sina.cn>
2018-11-02 15:00:32 +0800

    feat: add gateway

---
+++ fr-gateway/src/main/java/pers/guiwang/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package pers.guiwang.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author 鬼王
+ * @Date 2018/11/02 14:53
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
