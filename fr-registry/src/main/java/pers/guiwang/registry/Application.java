8600697af57a65e43c0600fa98b85775dd83c84d
Guiwang <guixiong97@sina.cn>
2018-11-02 14:12:51 +0800

    feat: add eureka server

---
+++ fr-registry/src/main/java/pers/guiwang/registry/Application.java
@@ -0,0 +1,19 @@
+package pers.guiwang.registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author 鬼王
+ * @Date 2018/11/02 14:04
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
