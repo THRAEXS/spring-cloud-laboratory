dd11bcdef19a786a59292f6d607ad118651bdc7f
Guiwang <guixiong97@sina.cn>
2018-11-05 17:41:39 +0800

    feat: determine config usage patterns

--- fr-config/src/test/java/pers/guiwang/config/ApplicationTests.java
+++ fr-config/src/test/java/pers/guiwang/config/ApplicationTests.java
@@ -1,13 +1,15 @@
 package pers.guiwang.config;
 
 import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * @Author 鬼王
  * @Date 2018/11/02 15:11
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApplicationTests {
 
 }
--- fr-registry/src/test/java/pers/guiwang/registry/ApplicationTests.java
+++ fr-registry/src/test/java/pers/guiwang/registry/ApplicationTests.java
@@ -7,18 +7,19 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
 
+
 /**
  * @Author 鬼王
  * @Date 2018/11/02 15:02
  */
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = DEFINED_PORT)
 public class ApplicationTests {
 
