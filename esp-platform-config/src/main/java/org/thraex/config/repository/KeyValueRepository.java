e9ad9cace5425601d040da42c0aa3d002f49501a
鬼王 <thraex@aliyun.com>
2019-06-10 11:43:28 +0800

    refactor: update groupId/artifactId/pakcage

---
+++ esp-platform-config/src/main/java/org/thraex/config/repository/KeyValueRepository.java
@@ -0,0 +1,18 @@
+package org.thraex.config.repository;
+
+import org.thraex.config.entity.KeyValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @Author 鬼王
+ * @Date 2018/11/27 14:32
+ */
+public interface KeyValueRepository extends JpaRepository<KeyValue, String> {
+
+    List<KeyValue> findByPidOrderByPkeyAscPvalueAsc(String pid);
+
+    void deleteByPid(String pid);
+
+}
