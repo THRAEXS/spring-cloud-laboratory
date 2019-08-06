0128ced6a49d7b5e56dd3d0809070a965ff1a61a
鬼王 <THRAEX@aliyun.com>
2019-08-06 17:32:48 +0800

    feat: add static resources for svg

---
+++ esp-framework-admin/src/main/java/org/thraex/admin/config/WebConfig.java
@@ -0,0 +1,43 @@
+package org.thraex.admin.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author 鬼王
+ * @Date 2019/08/06 14:44
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    /*private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source  = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }*/
+
+    //@Override
+    //public void addCorsMappings(CorsRegistry registry) {
+    //    registry.addMapping("/**")
+    //        .allowedOrigins("*")
+    //        .allowedMethods("*");
+    //}
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/svg/**")
+            .addResourceLocations("file:///Users/Guiwang/Workspace/JavaScripts/Notes/source/images/");
+    }
+
+}
