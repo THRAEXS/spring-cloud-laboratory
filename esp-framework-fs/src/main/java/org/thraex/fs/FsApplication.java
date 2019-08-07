0a51de562b0686bafd79025340505ab66525f773
鬼王 <THRAEX@aliyun.com>
2019-08-07 17:00:19 +0800

    feat: add module for fs

---
+++ esp-framework-fs/src/main/java/org/thraex/fs/FsApplication.java
@@ -0,0 +1,17 @@
+package org.thraex.fs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author 鬼王
+ * @Date 2019/08/07 16:55
+ */
+@SpringBootApplication
+public class FsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsApplication.class, args);
+    }
+
+}
