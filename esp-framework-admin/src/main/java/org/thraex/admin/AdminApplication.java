b4e5e2c45995f3356aa9f4b84dfbc78815094609
鬼王 <THRAEX@aliyun.com>
2019-08-14 14:41:42 +0800

    test: set banner

--- esp-framework-admin/src/main/java/org/thraex/admin/AdminApplication.java
+++ esp-framework-admin/src/main/java/org/thraex/admin/AdminApplication.java
@@ -1,7 +1,12 @@
 package org.thraex.admin;
 
+import org.springframework.boot.ImageBanner;
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 
 /**
  * @Author 鬼王
@@ -10,8 +15,44 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdminApplication {
 
+    /**
+     * Way 1:
+     * <PRE>
+     * SpringApplication.run(AdminApplication.class, args);
+     * </PRE>
+     *
+     * Way 2:
+     *
+     * <PRE>
+     * SpringApplication app = new SpringApplication(AdminApplication.class);
+     * app.run(args);
+     * </PRE>
+     *
+     * Way 3:
+     *
+     * <PRE>
+     * SpringApplicationBuilder().sources(AdminApplication.class).run(args);
+     *
+     * // or
+     *
+     * // 创建多层次的ApplicationContext
+     * new SpringApplicationBuilder()
+     *     .sources(AdminParent.class)
+     *     .child(AdminApplication.class)
+     *     .run(args);
+     * </PRE>
+     */
     public static void main(String[] args) {
-        SpringApplication.run(AdminApplication.class, args);
+        SpringApplication app = new SpringApplication(AdminApplication.class);
+
+        DefaultResourceLoader resourceLoader = new DefaultResourceLoader();
+        Resource resource = resourceLoader.getResource("public/sublime.jpg");
+        app.setBanner(new ImageBanner(resource));
+
+        //Resource resource = resourceLoader.getResource("public/app-5.svg");
+        //app.setBanner(new ResourceBanner(resource));
+
+        app.run(args);
     }
 
 }
