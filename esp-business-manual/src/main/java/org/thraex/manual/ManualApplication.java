8db9d26ef06babc0be23c546428ad91583300c2f
鬼王 <THRAEX@aliyun.com>
2019-08-15 20:23:48 +0800

    test: netflix-eureka-client for spring-cloud-starter-thraex

--- esp-business-manual/src/main/java/org/thraex/manual/ManualApplication.java
+++ esp-business-manual/src/main/java/org/thraex/manual/ManualApplication.java
@@ -1,9 +1,12 @@
 package org.thraex.manual;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.thraex.spring.cloud.sample.service.ThraexService;
 
 /**
  * @Author 鬼王
@@ -17,13 +20,13 @@ public class ManualApplication {
         SpringApplication.run(ManualApplication.class, args);
     }
 
-    //@Autowired
-    //private ThraexService service;
-    //
-    //@GetMapping("test/{name}")
-    //public String test(@PathVariable String name) {
-    //    return service.test(name);
-    //}
+    @Autowired
+    private ThraexService service;
+
+    @GetMapping("test/{name}")
+    public String test(@PathVariable String name) {
+        return service.test(name);
+    }
 
     @GetMapping("test")
     public String test() {
